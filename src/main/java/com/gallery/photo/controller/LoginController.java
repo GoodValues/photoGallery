626aea9cb4b342c762cb7771d9c554231dc246a4
Adam Niewiadomski <a.niewiadomski@onet.pl>
2021-02-04 16:22:12 +0100

    configuration, fixing entities, login controller, connecting with db

--- src/main/java/com/gallery/photo/controller/LoginController.java
+++ src/main/java/com/gallery/photo/controller/LoginController.java
@@ -4,17 +4,22 @@ import com.gallery.photo.configuration.jwt.JwtProvider;
 import com.gallery.photo.message.request.LoginDTO;
 import com.gallery.photo.message.response.JwtTokenDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class LoginController {
 
     JwtProvider provider;
@@ -26,8 +31,9 @@ public class LoginController {
         this.manager = manager;
     }
 
+    @ResponseBody
     @PostMapping("/signin")
-    public ResponseEntity authenticateUser(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity getTokenForUser(@RequestBody LoginDTO loginDTO){
 
         Authentication authentication =
                 manager.authenticate(new UsernamePasswordAuthenticationToken(
@@ -38,6 +44,19 @@ public class LoginController {
 
         return ResponseEntity.ok(new JwtTokenDTO(token));
 
+    }
+
+    @PostMapping(value = "/login", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public String getTokenForUser(@ModelAttribute("user") LoginDTO loginDTO, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        Authentication authentication = manager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.getUsername(),
+                        loginDTO.getPassword() )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = provider.generateToken(authentication);
+        redirectAttributes.addFlashAttribute("user", loginDTO);
+        return "redirect:/";
     }
 
 }
